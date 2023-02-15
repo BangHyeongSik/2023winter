5ef206ec9ef5faae483877d9dbed1597cfcffbc5
BangHyeongSik <bhs2874@naver.com>
2023-02-16 07:00:20 +0900

    20230216-Update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 let controllerData = require('./controller.json');
 
-app.use(':3000/public', express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/data', (req, res) => {
 	res.sendFile(__dirname + '/data.html');
